5ad7c34b46fd63624ec8421c83529d9503f84a4f
msa1709 <aravindvivek88@gmail.com>
2026-03-23 22:52:07 +0530

    Initial commit

---
+++ backend/api/views.py
@@ -0,0 +1,38 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+import os
+
+from groq import Groq 
+
+
+
+
+# get API key from environment variable
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+client = Groq(api_key="GROQ_API_KEY")
+
+@csrf_exempt
+def chat(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            message = data.get("message")
+
+            completion = client.chat.completions.create(
+               model="llama-3.1-8b-instant",
+                messages=[
+                     {"role": "system", "content": "You are a friendly and casual assistant. Talk like a human, not like a robot."},
+                    {"role": "user", "content": message}]
+            )
+
+            reply = completion.choices[0].message.content
+
+            return JsonResponse({"reply": reply})
+
+        except Exception as e:
+            return JsonResponse({"reply": str(e)})  
+    
+    # ✅ Handle GET request
+    return JsonResponse({"message": "API is working. Use POST request."})
