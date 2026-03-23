5d522923683b134cba9505da980b8ae4d7803de0
msa1709 <aravindvivek88@gmail.com>
2026-03-24 00:14:31 +0530

    updated code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://chatbot-grokchat-3.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
